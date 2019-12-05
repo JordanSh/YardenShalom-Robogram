f1c52904036b40767535a303ee6cc202a00bc7cc
Yarden Shalom <Yardeninho@gmail.com>
2019-12-05 13:33:39 +0200

    added git link

--- src/view/Credits/index.jsx
+++ src/view/Credits/index.jsx
@@ -7,6 +7,7 @@ import { FaLinkedin } from "react-icons/fa";
 import { FaWhatsapp } from "react-icons/fa";
 import { AiOutlineMail } from "react-icons/ai";
 import Techs from "./Techs";
+// add git link
 
 const Credits = () => {
   const createMaker = ({ name, gitHub, linkdin, email, phone }) => {
